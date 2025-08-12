a0a0fe3b02be3f2799a945580699d094d9fc8554
jleena <leena.j@altiushub.com>
2025-08-12 12:16:05 +0530

    Create a function which returns a random object of interface Car

---
+++ reactive-programming/exercise/index.ts
@@ -0,0 +1,25 @@
+import { Observable, Subject } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
+interface Car {
+    name: string;
+    model: string;
+    yearOfRelease: number;
+    brand: string;
+    color: string;
+}
+
+function randomCar(): Car {
+  const names = ['Speedster', 'Roadster', 'Cruiser', 'Phantom', 'Falcon'];
+  const models = ['X1', 'GT', 'Turbo', 'Z', 'RS'];
+  const brands = ['Tesla', 'Ford', 'BMW', 'Toyota', 'Honda', 'Hyundai', 'Scoda'];
+  const colors = ['black', 'white', 'red', 'blue', 'grey'];
+
+  return {
+    name: names[Math.floor(Math.random() * names.length)],
+    model: models[Math.floor(Math.random() * models.length)],
+    yearOfRelease: 2000 + Math.floor(Math.random() * 25),
+    brand: brands[Math.floor(Math.random() * brands.length)],
+    color: colors[Math.floor(Math.random() * colors.length)],
+  };
+}
